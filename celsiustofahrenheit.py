11ecb991d6df9233370f9246fd0d6230badf871b
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-01-23 14:49:02 -0500

    Create celsiustofahrenheit.py

---
+++ celsiustofahrenheit.py
@@ -0,0 +1,4 @@
+# To change the celsius, change the 38.8 to what you want.
+celsius = 38.8
+fahrenheit = (celsius * 1.8) + 32
+print(celsius, "Degrees celsius is", fahrenheit, "degrees fahrenheit.")
