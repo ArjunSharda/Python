e536165a85eed9c11544fc3ba30c6c588d12e833
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-02-04 19:08:02 -0500

    Create math.pownumber3.py

---
+++ math.pownumber3.py
@@ -0,0 +1,8 @@
+# You must import math to do this. To do so, type import math.
+# Numbers are 1-5 (basically at lines 4-8 at the end you can see its only 1-5.
+number3 = 3
+print(math.pow(number3, 1))
+print(math.pow(number3, 2))
+print(math.pow(number3, 3))
+print(math.pow(number3, 4))
+print(math.pow(number3, 5))
