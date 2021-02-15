58ff6e644bfa3caf97db6392040bedeaf5c89280
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-02-15 18:37:37 -0500

    Create squareimage.py

---
+++ squareimage.py
@@ -0,0 +1,13 @@
+# Turtle is what is used to make the square.
+import turtle
+t = turtle.Turtle()
+r = int(input("Enter how long you want the square to be:"))
+t.forward(r)
+t.left(90)
+t.forward(r)
+t.left(90)
+t.forward(r)
+t.left(90)
+t.forward(r)
+t.left(90)
+# Credits to https://www.geeksforgeeks.org/draw-square-and-rectangle-in-turtle-python/
