c344b7cc4c3972b5e23bd66283ca3528c3db5e45
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-02-16 19:24:52 -0500

    Create perimeterofrectangle.py

---
+++ perimeterofrectangle.py
@@ -0,0 +1,5 @@
+length = 10
+width = 20
+perimeter = 2 * (length+width)
+print(perimeter, "Is the answer with the current length and width given.")
+# Credit to https://www.tutorialgateway.org/python-program-to-find-perimeter-of-a-rectangle-using-length-and-width/ for the code.
