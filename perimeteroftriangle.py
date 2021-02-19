8de6cdbcf94c3ab87f4daac837d0d40d58c71f1a
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-02-18 20:04:57 -0500

    Create perimeteroftriangle.py

---
+++ perimeteroftriangle.py
@@ -0,0 +1,7 @@
+# Credit to google for the triangle perimeter (a, b, c), search triangle perimeter if you wish to see it.
+# Any information on google will be on here too. If google shows a question mark and you run this, this will return the wrong perimeter.
+# To find the information on google, search "triangle perimeter".
+a = 20
+b = 20
+c = 20
+print(a+b+c, "Is the perimeter of the triangle.")
