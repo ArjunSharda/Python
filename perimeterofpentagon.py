146c34e1b03ecfb848031679e15510e51870c0ee
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-02-20 12:22:35 -0500

    Create perimeterofpentagon.py

---
+++ perimeterofpentagon.py
@@ -0,0 +1,6 @@
+# Note that the perimeter may be wrong.
+# Search on google "perimeter of pentagon" and then type the value you put here.
+# If the answer does not match with the google answer, the perimeter here may be wrong.
+number = 5
+perimeter = number*5
+print(perimeter, "Is the perimeter of the number:", number)
