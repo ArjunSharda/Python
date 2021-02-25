13cf6143dc15d8acd994b92ff435f5e7a27a7c51
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-02-24 20:51:52 -0500

    Create perimeteroftrapezoid.py

---
+++ perimeteroftrapezoid.py
@@ -0,0 +1,8 @@
+# Credit to google for the a, b, c, d
+# Check google to see if the answer you type here is accurate. This could be inaccurate.
+a = 10
+b = 10
+c = 50
+d = 50
+perimeter = a+b+c+d
+print(perimeter, "Is the perimeter with the data entered.")
