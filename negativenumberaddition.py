546bfb5f631c15e51f92872b2a774e2ded168048
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-02-27 11:31:50 -0500

    Create negativenumberaddition.py

---
+++ negativenumberaddition.py
@@ -0,0 +1,4 @@
+# Credit to https://www.digitalocean.com/community/tutorials/how-to-do-math-in-python-3-with-operators
+negativenumber1 = -10
+negativenumber2 = 20
+print(negativenumber1+negativenumber2)
