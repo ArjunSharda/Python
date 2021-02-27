268bcda5fbd260820ef16871788010c92ab06b9e
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-02-27 11:28:04 -0500

    Create negativenumbersubstraction.py

---
+++ negativenumbersubstraction.py
@@ -0,0 +1,4 @@
+# Credit to https://www.digitalocean.com/community/tutorials/how-to-do-math-in-python-3-with-operators for the code
+negativenumber1 = -5
+negativenumber2 = -4
+print(-negativenumber1+-negativenumber2)
