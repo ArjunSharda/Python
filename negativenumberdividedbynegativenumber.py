fc4242073600743590cf6f75f6fa192ff2439be0
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-02-28 10:52:13 -0500

    Create negativenumberdividedbynegativenumber.py

---
+++ negativenumberdividedbynegativenumber.py
@@ -0,0 +1,5 @@
+# Credit to https://www.digitalocean.com/community/tutorials/how-to-do-math-in-python-3-with-operators for the tutorial
+# Credit to Math With Mr. J (on youtube) for the problem idea
+number1 = -20
+number2 = -2
+print(+number1/+number2)
