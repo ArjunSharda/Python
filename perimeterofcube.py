cf24e22d81ade4d1bf42698babe1c7338491e09e
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-03-01 21:12:25 -0500

    Create perimeterofcube.py

---
+++ perimeterofcube.py
@@ -0,0 +1,5 @@
+# Credit to https://www.reference.com/world-view/perimeter-cube-7e84a891eddf4a2c for the values names (inch and edge)
+inch = 10
+edge = 10
+perimeter = inch*edge
+print(perimeter, "Is the perimeter of the cube using the data entered.")
