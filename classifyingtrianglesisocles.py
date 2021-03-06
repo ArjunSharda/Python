1e0be86a796390cfa43dd7f51e332af9c3059124
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-03-06 16:35:07 -0500

    Create classifyingtrianglesisocles.py

---
+++ classifyingtrianglesisocles.py
@@ -0,0 +1,4 @@
+# Credit to Math with Mr. J (https://www.youtube.com/watch?v=yexIg1GtAG4) for the value name idea, in his video at 0:05
+trianglesides1and2 = 5
+triangleside3 = 2
+print("The triangle is a Iscocele, with the sides of", trianglesides1and2, "and", triangleside3)
