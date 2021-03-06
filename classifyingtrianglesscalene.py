c3576ac6c2fd64f887165fe97afd7a930de4c168
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-03-06 16:39:12 -0500

    Create classifyingtrianglesscalene.py

---
+++ classifyingtrianglesscalene.py
@@ -0,0 +1,5 @@
+# Credit to Math with Mr. J (https://www.youtube.com/watch?v=yexIg1GtAG4) for the value name idea, in his video at 0:05
+triangleside1scalene = 6
+triangleside2scalene = 5
+triangleside3scalene = 4
+print("The triangle is a Scalene, with the sides of", triangleside1scalene, "and", triangleside2scalene, "and also", triangleside3scalene)
