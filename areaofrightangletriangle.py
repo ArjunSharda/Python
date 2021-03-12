17dc06b0c9e3fdd53c37ac1913212bb968428b69
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2021-03-11 21:00:27 -0500

    Create areaofrightangletriangle.py

---
+++ areaofrightangletriangle.py
@@ -0,0 +1,5 @@
+a = 30
+b = 4
+areaofrightangletrianglenotdivided = a*b
+areaofrightangletriangle = areaofrightangletrianglenotdivided/2
+print(areaofrightangletriangle, "Is the area of the right angle triangle with the information entered.")
