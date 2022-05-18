be78a427c2ec02341388e44c5d6431649a4b65a9
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2022-05-18 10:45:50 -0500

    Create defnumberlessthancheck.py

---
+++ defnumberlessthancheck.py
@@ -0,0 +1,9 @@
+def test(test):
+    if test < 10:
+        print("This number is less than 10")
+    if test < 50:
+        print("This number is less than 50")
+    if test < 100:
+        print("This number is less than 100")
+
+test(25)
