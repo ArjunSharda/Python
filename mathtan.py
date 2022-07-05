37e097960ce234f7f83e6e43d65b2ab0191ba0d7
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2022-07-04 21:22:05 -0500

    Update mathtan.py

--- mathtan.py
+++ mathtan.py
@@ -1,4 +1,4 @@
-# This returns the tanget of the number
-# To do this you must import math. To do so, type import math.
+# This returns the tanget of the number specified
+import math
 mathtan = 15
 print (math.tan(mathtan))
