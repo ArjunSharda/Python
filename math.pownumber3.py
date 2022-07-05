d2ea1a89eabf0f3da3483dc7b16723a92011da98
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2022-07-04 21:20:52 -0500

    Update math.pownumber3.py

--- math.pownumber3.py
+++ math.pownumber3.py
@@ -1,4 +1,4 @@
-# You must import math to do this. To do so, type import math.
+import math
 # Numbers are 1-5 (basically at lines 4-8 at the end you can see its only 1-5.
 number3 = 3
 print(math.pow(number3, 1))
